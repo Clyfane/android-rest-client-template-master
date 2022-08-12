54618bd1af3e9dca50f115bdb85ccc94d79f09e4
Clyfane <107255231+Clyfane@users.noreply.github.com>
2022-08-11 22:35:53 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/Timeline.java
@@ -0,0 +1,144 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.codepath.apps.restclienttemplate.models.SampleModelDao;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class Timeline extends AppCompatActivity {
+
+    TwitterClient client;
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetsAdapter adapter;
+    SwipeRefreshLayout swipeContainer;
+    EndlessRecyclerViewScrollListener scrollListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        SampleModelDao sampleModelDao = ((TwitterApp) getApplicationContext()).getMyDatabase().sampleModelDao();
+
+
+
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("DB", "Database");
+            }
+        });
+
+        client = TwitterApp.getRestClient(this);
+
+
+        rvTweets = findViewById(R.id.rvTweets);
+        swipeContainer = findViewById(R.id.swipeContainer);
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i("TimelineActivity", "fetching new data" );
+                populateHomeTimeline();
+            }
+        });
+
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvTweets.setLayoutManager(layoutManager);
+        rvTweets.setAdapter(adapter);
+
+        rvTweets.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Log.i("TimelineActivity", "Load More Data");
+                loadMoreData();
+            }
+
+        };
+
+        // Adds the scroll listener to RecyclerView
+        rvTweets.addOnScrollListener(scrollListener);
+
+        populateHomeTimeline();
+
+    }
+
+    private void loadMoreData(){
+        client.getNextPageOfTweets(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i("TimelineActivity", "On Success for Load More Data 2" );
+
+                JSONArray jsonArray =json.jsonArray;
+                try {
+                    List<Tweet> tweets = Tweet.fromJsonArray(jsonArray);
+                    adapter.addAll(tweets);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e("TimelineActivity", "On Failure for Load More Data" + throwable);
+
+            }
+        }, tweets.get(tweets.size() -1).id );
+
+    }
+
+    public void populateHomeTimeline(){
+
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d("LoadOfData","OnSuccess"+ json.toString() );
+
+                JSONArray jsonArray = json.jsonArray;
+
+                try {
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    swipeContainer.setRefreshing(false);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d("OK","OnFaillure"+ response.toString());
+            }
+        });
+    }
+}
